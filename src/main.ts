729bdae1fd6d047aad602ce67281da21ab35a9c2
pihedy <pihedy@gmail.com>
2025-03-11 21:29:25 +0100

    Add icons and google analytics.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@
 
 import { createApp } from 'vue';
 
+import VueGtag from "vue-gtag-next";
+
 import router from './router/index';
 
 import App from './components/App.vue';
@@ -13,5 +15,9 @@ import './styles/main.scss';
 const app = createApp(App);
 
 app.use(router);
+
+app.use(VueGtag, {
+    property: {id: 'G-QNW20E0JM9'}
+});
 
 app.mount('#app');
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 
+import { Pickaxe } from 'lucide-vue-next';
+
 import ProjectList from '@/elements/global/ProjectList.vue';
 
 import projects from '@/data/projects.json';
@@ -7,5 +9,14 @@ import projects from '@/data/projects.json';
 </script>
 
 <template>
+    <header class="space-y-4">
+        <h1 class="text-2xl font-bold flex items-center gap-3">
+            <Pickaxe class="w-6 h-6" />
+            Projects
+        </h1>
+
+        <p class="text-slate-400 max-w-2xl">I work on my leisure projects using new technologies. Check out my GitHub.</p>
+    </header>
+
     <ProjectList :projects="projects" :featured="false" />
 </template>
